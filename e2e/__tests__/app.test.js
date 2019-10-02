f0c4fc899599fe92cd1ce0767742e6e3f113880b
Sam Jespersen <heyheysamj@gmail.com>
2019-10-01 22:22:03 -0700

    more setup

--- e2e/__tests__/app.test.js
+++ e2e/__tests__/app.test.js
@@ -1,8 +1,7 @@
 const request = require('../request');
 
 describe('core app api', () => {
-  it
-  ('is alive', () => {
+  it('is alive', () => {
     return request
       .get('/hello')
       .expect(200)
