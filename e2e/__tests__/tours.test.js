44ecd2d98cc076c2c278df6cf1483841fa1edf54
Sam Jespersen <heyheysamj@gmail.com>
2019-10-01 22:45:47 -0700

    tour post route

---
+++ e2e/__tests__/tours.test.js
@@ -0,0 +1,35 @@
+const request = require('../request');
+const db = require('../db');
+
+describe('tour api', () => {
+  beforeEach(() => {
+    return db.dropCollection('tours');
+  });
+
+  const tour = {
+    title: 'A Grand Tour',
+    activities: ['moshpit', 'ballpit'],
+    launchDate: new Date('4/20/2019'),
+    stops: []
+  };
+
+  function postTour(tour) {
+    return request
+      .post('/api/tours')
+      .send(tour)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('post a tour', () => {
+    return postTour(tour)
+      .then(tour => {
+        expect(tour).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          ...tour
+        });
+      });
+  });
+
+});
